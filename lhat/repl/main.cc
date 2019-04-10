4726e5eb7e398257c0cbf9840c0ea12da3210267
Stanislav Gatev <sg@sgatev.com>
2019-04-10 18:43:15 +0300

    Reduce duplication in repl code

--- lhat/repl/main.cc
+++ lhat/repl/main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <utility>
-#include <vector>
 
 #include "lhat/named/ast.h"
 #include "lhat/named/parser.h"
@@ -16,55 +16,56 @@
 namespace lhat {
 namespace repl {
 namespace {
+std::pair<std::string, std::string> ReadCommand(std::string input) {
+  return absl::StrSplit(input, absl::MaxSplits(' ', 1));
+}
+
 void Run() {
   ConstEnv consts;
-  std::string input;
+  std::string input, command;
   while (true) {
     std::getline(std::cin, input);
+    std::tie(command, input) = ReadCommand(input);
 
-    const std::vector<std::string> input_parts =
-        absl::StrSplit(input, absl::MaxSplits(' ', 1));
-    const std::string& command = input_parts[0];
+    auto with_input_term = [&consts, &input](auto&& handle_term) {
+      consts.Resolve(&input);
+      const named::Term input_term = named::Parser::Parse(input);
+
+      NameContext free_nctx;
+      nameless::Term term = RemoveNames(input_term, &free_nctx);
+
+      handle_term(&term, &free_nctx);
+    };
 
     if (command == "exit") {
       break;
     } else if (command == "def") {
-      consts.Set(absl::StrSplit(input_parts[1], absl::MaxSplits(' ', 1)));
+      consts.Set(absl::StrSplit(input, absl::MaxSplits(' ', 1)));
     } else if (command == "beta-redex?") {
-      input = input_parts[1];
-
-      consts.Resolve(&input);
-      const named::Term input_term = named::Parser::Parse(input);
-
-      NameContext free_nctx;
-      nameless::Term term = RemoveNames(input_term, &free_nctx);
-
-      std::cout << std::boolalpha << nameless::IsBetaRedex(term) << std::endl;
+      with_input_term(
+          [](const nameless::Term* term, const NameContext* free_nctx) {
+            std::cout << std::boolalpha << nameless::IsBetaRedex(*term)
+                      << std::endl;
+          });
     } else if (command == "normal-form?") {
-      input = input_parts[1];
+      with_input_term(
+          [](const nameless::Term* term, const NameContext* free_nctx) {
+            std::cout << std::boolalpha << nameless::IsNormalForm(*term)
+                      << std::endl;
+          });
+    } else if (command == "normalize-appl") {
+      with_input_term([](nameless::Term* term, NameContext* free_nctx) {
+        while (nameless::BetaReduceAppl(term)) {
+          // Normalize the term.
+        }
 
-      consts.Resolve(&input);
-      const named::Term input_term = named::Parser::Parse(input);
-
-      NameContext free_nctx;
-      nameless::Term term = RemoveNames(input_term, &free_nctx);
-
-      std::cout << std::boolalpha << nameless::IsNormalForm(term) << std::endl;
+        const named::Term output_term = AddNames(*term, free_nctx);
+        std::string output;
+        named::Printer::Print(output_term, &output);
+        std::cout << output << std::endl;
+      });
     } else {
-      consts.Resolve(&input);
-      const named::Term input_term = named::Parser::Parse(input);
-
-      NameContext free_nctx;
-      nameless::Term term = RemoveNames(input_term, &free_nctx);
-
-      while (nameless::BetaReduceAppl(&term)) {
-        // Reduce the term.
-      }
-
-      const named::Term output_term = AddNames(term, &free_nctx);
-      std::string output;
-      named::Printer::Print(output_term, &output);
-      std::cout << output << std::endl;
+      std::cout << "Unknown command: " << command << std::endl;
     }
   }
 }
