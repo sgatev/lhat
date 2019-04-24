da4e507134d1c19e420f590bf9ba65b4228c4e39
Stanislav Gatev <sg@sgatev.com>
2019-04-24 23:46:14 +0300

    Replace IsWhitespace function with std::isspace

--- lhat/util/parse.cc
+++ lhat/util/parse.cc
@@ -1,13 +1,11 @@
 #include "lhat/util/parse.h"
 
+#include <cctype>
+
 namespace lhat {
 namespace util {
-namespace {
-bool IsWhitespace(char c) { return c == ' ' || c == '\t' || c == '\n'; }
-}  // namespace
-
 void DiscardWhitespace(std::istream* input) {
-  while (!input->eof() && IsWhitespace(input->peek())) {
+  while (!input->eof() && std::isspace(input->peek())) {
     input->get();
   }
 }
