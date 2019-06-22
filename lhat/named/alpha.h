afaa34fc6292e10b3a1f8e3b3ac7543ff75fdcef
Stanislav Gatev <sg@sgatev.com>
2019-06-22 21:53:07 +0300

    Fix typo

--- lhat/named/alpha.h
+++ lhat/named/alpha.h
@@ -5,7 +5,7 @@
 
 namespace lhat {
 namespace named {
-// Returns true if ther terms m and n are alpha-equivalent.
+// Returns true if the terms m and n are alpha-equivalent.
 bool IsAlphaEquiv(const Term& m, const Term& n);
 }  // namespace named
 }  // namespace lhat
