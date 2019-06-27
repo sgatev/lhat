b847b5ecd68902c2c9b7307604d2a617d9547a4d
Stanislav Gatev <sg@sgatev.com>
2019-06-27 14:00:28 +0300

    Reword comments in named::SafeSub

--- lhat/named/sub.cc
+++ lhat/named/sub.cc
@@ -10,23 +10,26 @@ void SafeSub(const std::string& var_name, const Term& replacement,
              Term* target) {
   target->Match(
       [&var_name, &replacement](Abst& abst) {
-        if (abst.VarName() == var_name)
-          // no need to rename in this case, just do nothing
+        if (abst.VarName() == var_name) {
+          // no need to substitute in this case
           return;
+        }
 
         std::unordered_set<std::string> body_free_var_names;
         InsertFreeVarNames(abst.Body(), &body_free_var_names);
 
-        if (body_free_var_names.find(var_name) == body_free_var_names.end())
-          // also no need to execute the substitution in this case
+        if (body_free_var_names.find(var_name) == body_free_var_names.end()) {
+          // no need to substitute in this case
           return;
+        }
 
         std::unordered_set<std::string> replacement_free_var_names;
         InsertFreeVarNames(replacement, &replacement_free_var_names);
 
         if (replacement_free_var_names.find(abst.VarName()) !=
-                 replacement_free_var_names.end()) {
-          // case in which renaming is needed
+            replacement_free_var_names.end()) {
+          // rename the variable name in order to ensure collision-free
+          // substitution
           std::unordered_set<std::string> var_names;
           InsertVarNames(abst.Body(), &var_names);
           InsertVarNames(replacement, &var_names);
@@ -35,6 +38,7 @@ void SafeSub(const std::string& var_name, const Term& replacement,
           Sub(abst.VarName(), Term(Var(new_var_name)), abst.MutableBody());
           abst.SetVarName(new_var_name);
         }
+
         SafeSub(var_name, replacement, abst.MutableBody());
       },
       [&var_name, &replacement](Appl& appl) {
